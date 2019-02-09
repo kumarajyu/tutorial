87b6939fcd8b74cab6e86e3de3cb60547bcd4861
宮岳誓吾 <kumarajyu0l@gmail.com>
2019-02-09 12:17:38 +0000

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,10 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
+  end
+  
+  def create
     @user = User.new(user_params)
     if @user.save
       # 保存の成功をここで扱う。
@@ -18,5 +22,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-    end  
+    end
 end
