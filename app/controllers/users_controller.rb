b9aadc209fe59350b732e69523d4e59a4e7fe49b
宮岳誓吾 <kumarajyu0l@gmail.com>
2019-03-01 19:15:43 +0000

    10.1.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,15 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合を扱う。
+    else
+      render 'edit'
+    end
+  end
 
   private
 
