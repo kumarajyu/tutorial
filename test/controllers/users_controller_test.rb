f0a46e41237f35bf626ba27bf6ce12fea26b762c
宮岳誓吾 <kumarajyu0l@gmail.com>
2019-03-04 08:26:39 +0000

    10.4.2

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -12,6 +12,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
   
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              @other_user.password,
+                                            password_confirmation: @other_user.password,
+                                            admin: true } }
+    assert_not @other_user.relord.admin?
+  end
+  
   test "should get new" do
     get signup_path
     assert_response :success
