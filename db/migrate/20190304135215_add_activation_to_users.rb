5b1d138fef3ea8656fcff1cd2d42bc713156d5d9
宮岳誓吾 <kumarajyu0l@gmail.com>
2019-03-04 15:16:12 +0000

    11.3 green

--- db/migrate/20190304135215_add_activation_to_users.rb
+++ db/migrate/20190304135215_add_activation_to_users.rb
@@ -1,13 +1,5 @@
-class AddActivationToUsers < ActiveRecord::Migration[5.1]
+class AddActivationToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :rails, :string
-    add_column :users, :generate, :string
-    add_column :users, :migration, :string
-    add_column :users, :add_activation_to_users, :string
-    add_column :users, :rails, :string
-    add_column :users, :generate, :string
-    add_column :users, :migration, :string
-    add_column :users, :add_activation_to_users, :string
     add_column :users, :activation_digest, :string
     add_column :users, :activated, :boolean, default: false
     add_column :users, :activated_at, :datetime
