759d6892ad7c97903033c114573ce4983667c39c
宮岳誓吾 <kumarajyu0l@gmail.com>
2019-03-05 12:58:55 +0000

    11.2.3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,10 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'f862037ecc7d4bf38863361d22e8aca5.vfs.cloud9.us-east-2.amazonaws.com'# ここをコピペすると失敗します。自分の環境に合わせてください。
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
