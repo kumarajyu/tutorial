02740093b87275441802ab270ed5f443cd89ad02
宮岳誓吾 <kumarajyu0l@gmail.com>
2019-03-06 12:23:29 +0000

    11.33green

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,10 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      if user.activated?
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back_or user
+      if @user.activated?
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
       else
         message  = "Account not activated. "
         message += "Check your email for the activation link."
@@ -21,9 +21,9 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def destroy
     log_out if logged_in?
-    redirect_to root_url    
+    redirect_to root_url
   end
 end
