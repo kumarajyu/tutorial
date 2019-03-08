3b3f1339294050a5934414c3fc9d75f2b88000d4
宮岳誓吾 <kumarajyu0l@gmail.com>
2019-03-08 11:19:11 +0000

    SendGrid Test2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,8 +88,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app122584588@heroku.com'],
-    :password       => ENV['pjcyvqxx2736'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
