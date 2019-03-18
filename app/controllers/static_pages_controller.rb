f534d5ed6a67412704f144e827d9d1dd4481b312
宮岳誓吾 <kumarajyu0l@gmail.com>
2019-03-18 05:33:36 +0000

    13.56 green

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   
   def home
     if logged_in?
-      @micropost = current_user.micropost.build
+      @micropost = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
