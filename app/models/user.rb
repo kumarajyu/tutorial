511ddb55c46db770a32cc4db1f290743e7721a15
宮岳誓吾 <kumarajyu0l@gmail.com>
2019-03-21 12:35:03 +0000

    14.2 green

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy                                
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -61,7 +65,7 @@ class User < ApplicationRecord
     update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
 
-  # パスワード再設定のメールを送信する
+  # パスワード再設定のメ���ルを送信する
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
