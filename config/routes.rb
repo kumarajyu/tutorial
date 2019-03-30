79206f87477aecdd6bd7905da5297a60e0bd948e
kumarajyu <kumarajyu0@gmail.com>
2019-03-30 15:01:53 +0000

    8.2 red

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
 
+  get 'rooms/show'
+
   get 'password_resets/new'
 
   get 'password_resets/edit'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190320105700) do
+ActiveRecord::Schema.define(version: 20190330132619) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
