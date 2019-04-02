346d8e866c3f0e923cf31679be3a21cd3940c1c3
kumarajyu <kumarajyu0@gmail.com>
2019-04-02 01:26:50 +0000

    speak action green

--- Gemfile
+++ Gemfile
@@ -13,8 +13,13 @@ gem 'sass-rails',   '5.0.6'
 gem 'uglifier',     '3.2.0'
 gem 'coffee-rails', '4.2.2'
 gem 'jquery-rails', '4.3.1'
+gem 'jquery-turbolinks', '2.1.0'
 gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.7.0'
+gem 'pry-rails'
+gem 'redis'
+gem 'redis-rails'
+
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- config/routes.rb
+++ config/routes.rb
@@ -28,4 +28,7 @@ Rails.application.routes.draw do
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
   resources :relationships,       only: [:create, :destroy]
+  
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
 end
