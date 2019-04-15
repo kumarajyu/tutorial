31a4b6d5d4b0ea80cfbcdaf3ed21618562e6fe33
kumarajyu <kumarajyu0@gmail.com>
2019-04-15 01:34:47 +0000

    10.34

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,8 @@
 class PicturesController < ApplicationController
   
+  def new
+  end
+  
   def create
     path = 'public/images/'
     picture = Picture.create
@@ -19,4 +22,39 @@ class PicturesController < ApplicationController
     render :nothing => true
   end
   
+  def index
+    ids = []
+    Picture.all.reverse.each do |picture|
+      ids << picture.id
+    end
+    @response = ids.join(',')
+  end
+
+  def list
+    ids = []
+    Picture.all.reverse.each do |picture|
+      ids << picture.id
+    end
+    # render :text => ids.join(',')
+
+    @response = ids.join(',')
+    respond_to do |format|
+      format.html # .html.erb
+      format.xml   { render xml: ids }
+      format.json  { render json: ids }
+      format.text  { render text: ids.join(',') }
+    end
+  end
+
+  # DELETE /picture/1
+  # DELETE /picture/1.json
+  def destroy
+    @pict = Picture.find(params[:id])
+    @pict.destroy if @pict
+
+    respond_to do |format|
+      format.html { redirect_to pictures_url }
+      format.json { head :no_content }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,16 @@ Rails.application.routes.draw do
 
   get 'sessions/new'
   
-  
-  
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
+  
   get '/pictures',  to: 'pictures#new'
+  get 'pictures/list',  to: 'pictures#list'
+  
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
+  
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
@@ -31,7 +33,8 @@ Rails.application.routes.draw do
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
   resources :relationships,       only: [:create, :destroy]
-  resources :pictures, only: [:create]
+  resources :pictures, only: [:create, :index, :destroy]
+ 
   
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
