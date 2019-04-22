dd4f3c710141ad4ce6dd7abeeecfe31b924c8d33
kumarajyu <kumarajyu0@gmail.com>
2019-04-22 08:03:51 +0000

    addcolumn

--- db/migrate/20190330132619_create_messages.rb
+++ db/migrate/20190330132619_create_messages.rb
@@ -2,8 +2,12 @@ class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
       t.text :content
+      t.integer :from_id
+      t.integer :to_id
+      t.string :room_id
 
       t.timestamps
     end
+    add_index :messages, [:room_id, :created_at]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190412131601) do
+ActiveRecord::Schema.define(version: 20190422073943) do
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "from_id"
   end
 
   create_table "microposts", force: :cascade do |t|
