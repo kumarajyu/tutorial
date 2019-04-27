bb225401face25c7ad6634cf98a296b204cc6640
kumarajyu <kumarajyu0@gmail.com>
2019-04-27 03:10:23 +0000

    mailer test

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,7 +65,7 @@ class User < ApplicationRecord
     update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
 
-  # パスワード再設定のメ���ルを送信する
+  # パスワード再設定のメールを送信する
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
